debfa05642154a5b90d1d384f0014ff082b139a2
Ashwini Iyer <bookworm.iyer@gmail.com>
2015-02-22 10:45:45 -0500

    thank you

--- Memory App/ViewController.swift
+++ Memory App/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("ashwini 1");
+        println("ashwini 2");
         
     }
 
